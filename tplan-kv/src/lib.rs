fdc66be595188a28aabf73e7de3ef487e8f98b28
Matt <1009003+tantaman@users.noreply.github.com>
2023-10-02 11:07:20 -0400

    impl in-mem, prep for lsm

--- tplan-kv/src/lib.rs
+++ tplan-kv/src/lib.rs
@@ -1,15 +1,31 @@
-pub struct KvStore {}
+use std::collections::HashMap;
+
+struct KVErr {
+    msg: String,
+}
+
+type KVResult<T> = Result<T, KVErr>;
+
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore {}
+        KvStore {
+            map: HashMap::new(),
+        }
     }
 
     pub fn get(&self, key: String) -> Option<String> {
-        None
+        self.map.get(&key).cloned()
     }
 
-    pub fn set(&mut self, key: String, value: String) {}
+    pub fn set(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+    }
 
-    pub fn remove(&mut self, key: String) {}
+    pub fn remove(&mut self, key: String) {
+        self.map.remove(&key);
+    }
 }
