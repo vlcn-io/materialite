d713644d424b05e6ae929800984b005c53763e8f
Matt <1009003+tantaman@users.noreply.github.com>
2023-10-02 11:11:19 -0400

    lsm tests

--- tplan-kv/src/lib.rs
+++ tplan-kv/src/lib.rs
@@ -1,31 +1,38 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::Path};
 
-struct KVErr {
+#[derive(Debug)]
+pub struct KVErr {
     msg: String,
 }
 
-type KVResult<T> = Result<T, KVErr>;
+pub type KVResult<T> = Result<T, KVErr>;
 
 pub struct KvStore {
     map: HashMap<String, String>,
 }
 
 impl KvStore {
+    pub fn open(path: &Path) -> KVResult<KvStore> {
+        Ok(KvStore::new())
+    }
+
     pub fn new() -> KvStore {
         KvStore {
             map: HashMap::new(),
         }
     }
 
-    pub fn get(&self, key: String) -> Option<String> {
-        self.map.get(&key).cloned()
+    pub fn get(&self, key: String) -> KVResult<Option<String>> {
+        Ok(self.map.get(&key).cloned())
     }
 
-    pub fn set(&mut self, key: String, value: String) {
+    pub fn set(&mut self, key: String, value: String) -> KVResult<()> {
         self.map.insert(key, value);
+        Ok(())
     }
 
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> KVResult<()> {
         self.map.remove(&key);
+        Ok(())
     }
 }
