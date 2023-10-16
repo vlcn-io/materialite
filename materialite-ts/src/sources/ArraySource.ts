821f85478288f261b552fbd85fe5f9266db9f7e1
Matt <1009003+tantaman@users.noreply.github.com>
2023-10-16 14:20:30 -0400

    remove sources -- we only need a set source is my belief

--- materialite-ts/src/sources/ArraySource.ts
+++
@@ -1,8 +0,0 @@
-/**
- * A source of values for incremental computation
- * based on an array.
- *
- * sorted array of indices for materialized view
- * multiset where index is identity?
- */
-export class ArraySource {}
--- materialite-ts/src/sources/MultimapSource.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Many values mapped to the same key.
- *
- * This is just a set of tuples where the first entry is the key, right?
- */
-// export class MultimapSource<K, V> {}
