ce53cf3f70531b41218676597658dd2cb23bd369
Matt <1009003+tantaman@users.noreply.github.com>
2023-10-18 23:56:58 -0400

    paginated react differential?

---
+++ materialite-demo/src/react-paginated-differential/TaskApp.tsx
@@ -0,0 +1,81 @@
+import React, { useMemo, useState } from "react";
+import { TaskTable } from "./TaskTable.js";
+import { TaskComponent } from "./Task.js";
+import { Task } from "../data/tasks/schema.js";
+import { createTasks } from "../data/tasks/createTasks.js";
+import { TaskFilter } from "./TaskFilter.js";
+
+const seedTasks = createTasks(1000000);
+export const TaskApp: React.FC = () => {
+  const [selectedTask, setSelectedTask] = useState<Task | null>(null);
+  const [allTasks, setAllTasks] = useState(seedTasks);
+  const [filter, setFilter] = useState<TaskFilter>({});
+
+  function onTaskSelected(task: Task) {
+    setSelectedTask(task);
+  }
+
+  function onTaskUpdated(task: Task) {
+    if (selectedTask == null) {
+      return;
+    }
+    if (selectedTask.id !== task.id) {
+      return;
+    }
+    // oof
+    const allTasksCopy: Task[] = [];
+    for (const t of allTasks) {
+      if (t.id === task.id) {
+        allTasksCopy.push(task);
+      } else {
+        allTasksCopy.push(t);
+      }
+    }
+    setAllTasks(allTasksCopy);
+    setSelectedTask(task);
+  }
+
+  const filteredTasks = useMemo(() => {
+    return filterTasks(filter, allTasks);
+  }, [allTasks, filter]);
+
+  return (
+    <div className="flex h-screen">
+      {/* Left Pane - Task Table */}
+      <div className="w-3/4 bg-gray-100 overflow-y-auto">
+        <TaskFilter onFilterChange={setFilter} />
+        <TaskTable
+          tasks={filteredTasks}
+          onTaskClick={onTaskSelected}
+          selectedTask={selectedTask != null ? selectedTask.id : undefined}
+        />
+      </div>
+      {/* Right Pane - Task Details */}
+      <div className="w-1/4 bg-white overflow-y-auto p-6">
+        {selectedTask ? (
+          <TaskComponent task={selectedTask} onTaskChanged={onTaskUpdated} />
+        ) : (
+          <div>Select a task to view details</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+function filterTasks(filter: TaskFilter, tasks: Task[]): Task[] {
+  return tasks.filter((task) => {
+    let keep = true;
+    for (const k in filter) {
+      const casted = k as keyof TaskFilter;
+      if (filter[casted] == null) {
+        continue;
+      }
+      keep = task[casted] === filter[casted];
+      if (!keep) {
+        return false;
+      }
+    }
+
+    return keep;
+  });
+}
---
+++ materialite-demo/src/react-paginated-differential/TaskTable.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Task } from "../data/tasks/schema.js";
+import { ListChildComponentProps } from "react-window";
+import { VirtualTable } from "../virtualized/VirtualTable.js";
+
+type TaskTableProps = {
+  tasks: Task[];
+  onTaskClick: (task: Task) => void;
+  selectedTask?: number;
+};
+
+export const TaskTable: React.FC<TaskTableProps> = (props) => {
+  return (
+    <div
+      className="bg-gray-100 p-6 overflow-y-auto"
+      style={{ position: "relative", top: 130, height: "calc(100vh - 130px)" }}
+    >
+      <VirtualTable
+        height={window.innerHeight - 200}
+        width="100%"
+        className="min-w-full bg-white rounded-md overflow-hidden"
+        itemCount={props.tasks.length}
+        itemData={props}
+        itemSize={50}
+        header={
+          <thead>
+            <tr>
+              <th className="text-left py-2 px-3 font-semibold">ID</th>
+              <th className="text-left py-2 px-3 font-semibold">Title</th>
+              <th className="text-left py-2 px-3 font-semibold">Assignee</th>
+              <th className="text-left py-2 px-3 font-semibold">Due Date</th>
+              <th className="text-left py-2 px-3 font-semibold">Status</th>
+              <th className="text-left py-2 px-3 font-semibold">Priority</th>
+              <th className="text-left py-2 px-3 font-semibold">Project</th>
+              <th className="text-left py-2 px-3 font-semibold">Labels</th>
+            </tr>
+          </thead>
+        }
+        row={Row}
+      />
+    </div>
+  );
+};
+
+function Row({ data, index }: ListChildComponentProps<TaskTableProps>) {
+  const task = data.tasks[index];
+  return (
+    <tr
+      key={task.id}
+      style={{ height: 50 }}
+      className={`border-t cursor-pointer ${
+        task.id === data.selectedTask ? "bg-blue-200" : "hover:bg-blue-100"
+      }`}
+      onClick={() => data.onTaskClick(task)}
+    >
+      <td className="py-2 px-3">{task.id}</td>
+      <td className="py-2 px-3">{task.title}</td>
+      <td className="py-2 px-3">{task.assignee}</td>
+      <td className="py-2 px-3">{task.dueDate.toISOString().split("T")[0]}</td>
+      <td className="py-2 px-3">{task.status}</td>
+      <td className="py-2 px-3">{task.priority}</td>
+      <td className="py-2 px-3">{task.project}</td>
+      <td className="py-2 px-3">{task.labels.join(", ")}</td>
+    </tr>
+  );
+}
