3cb4697602268f00b3c9b17ea18bf8240c8d1a9f
Matt <1009003+tantaman@users.noreply.github.com>
2023-11-12 13:16:38 -0500

    start working on a secondary example

---
+++ demos/react2/src/TaskTable.tsx
@@ -0,0 +1,221 @@
+import React from "react";
+
+import "./index.css";
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  Row,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useVirtual } from "react-virtual";
+import { Task } from "./data/tasks/schema";
+
+export default function TaskTable() {
+  const rerender = React.useReducer(() => ({}), {})[1];
+
+  //we need a reference to the scrolling element for logic down below
+  const tableContainerRef = React.useRef<HTMLDivElement>(null);
+
+  const columns = React.useMemo<ColumnDef<Task>[]>(
+    () => [
+      {
+        accessorKey: "id",
+        header: "ID",
+        size: 60,
+      },
+      {
+        accessorKey: "assignee",
+      },
+      {
+        accessorKey: "title",
+      },
+      {
+        accessorKey: "description",
+      },
+      {
+        accessorKey: "dueDate",
+        cell: (info) => info.getValue<Date>().toLocaleString(),
+        header: "Due Date",
+      },
+      {
+        accessorKey: "status",
+        header: "Status",
+      },
+      {
+        accessorKey: "priority",
+      },
+      {
+        accessorKey: "labels",
+        cell: (info) => info.getValue<string[]>().join(", "),
+      },
+      {
+        accessorKey: "project",
+      },
+    ],
+    []
+  );
+
+  const isFetching = false;
+  const isLoading = false;
+  function fetchNextPage() {}
+  // const { data, fetchNextPage, isFetching, isLoading } =
+  //   useInfiniteQuery<PersonApiResponse>(
+  //     ["table-data", sorting], //adding sorting state as key causes table to reset and fetch from new beginning upon sort
+  //     async ({ pageParam = 0 }) => {
+  //       const start = pageParam * fetchSize;
+  //       const fetchedData = fetchData(start, fetchSize, sorting); //pretend api call
+  //       return fetchedData;
+  //     },
+  //     {
+  //       getNextPageParam: (_lastGroup, groups) => groups.length,
+  //       keepPreviousData: true,
+  //       refetchOnWindowFocus: false,
+  //     }
+  //   );
+
+  //we must flatten the array of arrays from the useInfiniteQuery hook
+  const flatData: Task[] = [];
+  const totalDBRowCount = 0;
+  const totalFetched = flatData.length;
+
+  //called on scroll and possibly on mount to fetch more data as the user scrolls and reaches bottom of table
+  const fetchMoreOnBottomReached = React.useCallback(
+    (containerRefElement?: HTMLDivElement | null) => {
+      if (containerRefElement) {
+        const { scrollHeight, scrollTop, clientHeight } = containerRefElement;
+        //once the user has scrolled within 300px of the bottom of the table, fetch more data if there is any
+        if (
+          scrollHeight - scrollTop - clientHeight < 300 &&
+          !isFetching &&
+          totalFetched < totalDBRowCount
+        ) {
+          fetchNextPage();
+        }
+      }
+    },
+    [fetchNextPage, isFetching, totalFetched, totalDBRowCount]
+  );
+
+  //a check on mount and after a fetch to see if the table is already scrolled to the bottom and immediately needs to fetch more data
+  React.useEffect(() => {
+    fetchMoreOnBottomReached(tableContainerRef.current);
+  }, [fetchMoreOnBottomReached]);
+
+  const table = useReactTable({
+    data: flatData,
+    columns,
+    // state: {
+    //   sorting,
+    // },
+    // onSortingChange: setSorting,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
+  });
+
+  const { rows } = table.getRowModel();
+
+  //Virtualizing is optional, but might be necessary if we are going to potentially have hundreds or thousands of rows
+  const rowVirtualizer = useVirtual({
+    parentRef: tableContainerRef,
+    size: rows.length,
+    overscan: 10,
+  });
+  const { virtualItems: virtualRows, totalSize } = rowVirtualizer;
+  const paddingTop = virtualRows.length > 0 ? virtualRows?.[0]?.start || 0 : 0;
+  const paddingBottom =
+    virtualRows.length > 0
+      ? totalSize - (virtualRows?.[virtualRows.length - 1]?.end || 0)
+      : 0;
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+
+  return (
+    <div className="p-2">
+      <div className="h-2" />
+      <div
+        className="container"
+        onScroll={(e) => fetchMoreOnBottomReached(e.target as HTMLDivElement)}
+        ref={tableContainerRef}
+      >
+        <table>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <th
+                      key={header.id}
+                      colSpan={header.colSpan}
+                      style={{ width: header.getSize() }}
+                    >
+                      {header.isPlaceholder ? null : (
+                        <div
+                          {...{
+                            className: header.column.getCanSort()
+                              ? "cursor-pointer select-none"
+                              : "",
+                            onClick: header.column.getToggleSortingHandler(),
+                          }}
+                        >
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                          {{
+                            asc: " 🔼",
+                            desc: " 🔽",
+                          }[header.column.getIsSorted() as string] ?? null}
+                        </div>
+                      )}
+                    </th>
+                  );
+                })}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {paddingTop > 0 && (
+              <tr>
+                <td style={{ height: `${paddingTop}px` }} />
+              </tr>
+            )}
+            {virtualRows.map((virtualRow) => {
+              const row = rows[virtualRow.index] as Row<Task>;
+              return (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+            {paddingBottom > 0 && (
+              <tr>
+                <td style={{ height: `${paddingBottom}px` }} />
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        Fetched {flatData.length} of {totalDBRowCount} Rows.
+      </div>
+      <div>
+        <button onClick={() => rerender()}>Force Rerender</button>
+      </div>
+    </div>
+  );
+}
---
+++ demos/react2/src/data/music/createMusic.ts
@@ -0,0 +1,108 @@
+import { albums } from "./albums.js";
+import { artists } from "./artists.js";
+import { songs } from "./songs.js";
+import { playlists } from "./playlists.js";
+
+export function createMusic(
+  totalTracks: number,
+  totalAlbums: number,
+  totalArtists: number,
+  totalPlaylists: number
+) {
+  // const TOTAL_TRACKS = 10000;
+  // const TOTAL_ALBUMS = 500; // Assuming an average of 20 tracks per album
+  // const TOTAL_ARTISTS = 100;
+  // const TOTAL_PLAYLISTS = 250; // Assuming an average of 40 tracks per playlist
+  const TOTAL_TRACKS = totalTracks;
+  const TOTAL_ALBUMS = totalAlbums;
+  const TOTAL_ARTISTS = totalArtists;
+  const TOTAL_PLAYLISTS = totalPlaylists;
+
+  const tracks = [];
+  const albums = [];
+  const artists = [];
+  const playlists = [];
+  const playlistTracks = [];
+  const trackArtists = [];
+
+  for (let i = 1; i <= TOTAL_ARTISTS; i++) {
+    artists.push({
+      id: i,
+      name: makeArtistName(),
+    });
+  }
+
+  for (let i = 1; i <= TOTAL_ALBUMS; i++) {
+    albums.push({
+      id: i,
+      title: makeAlbumName(),
+      cover_art_url: `http://example.com/album${i}.jpg`,
+      released: 1609459200 + i * 86400, // A day apart for each release
+    });
+  }
+
+  for (let i = 1; i <= TOTAL_PLAYLISTS; i++) {
+    playlists.push({
+      id: i,
+      name: makePlaylistName(),
+    });
+  }
+
+  for (let i = 1; i <= TOTAL_TRACKS; i++) {
+    const albumId = 1 + Math.floor(Math.random() * TOTAL_ALBUMS);
+    const artistId = 1 + Math.floor(Math.random() * TOTAL_ARTISTS);
+
+    tracks.push({
+      id: i,
+      title: makeTrackName(),
+      duration: 200 + Math.floor(Math.random() * 300),
+      album_id: albumId,
+    });
+
+    trackArtists.push({
+      track_id: i,
+      artist_id: artistId,
+    });
+
+    const playlistId = 1 + Math.floor(Math.random() * TOTAL_PLAYLISTS);
+    playlistTracks.push({
+      playlist_id: playlistId,
+      track_id: i,
+    });
+  }
+
+  return {
+    tracks,
+    albums,
+    artists,
+    playlists,
+    playlistTracks,
+    trackArtists,
+  };
+}
+
+function makeAlbumName() {
+  makeName(albums[0], albums[1]);
+}
+
+function makeArtistName() {
+  makeName(artists[0], artists[1]);
+}
+
+function makeTrackName() {
+  makeName(songs[0], songs[1]);
+}
+
+function makePlaylistName() {
+  makeName(playlists[0], playlists[1]);
+}
+
+function makeName(arr1: string[], arr2: string[]) {
+  const idx1 = Math.floor(Math.random() * arr1.length);
+  const idx2 = Math.floor(Math.random() * arr2.length);
+  if (Math.random() < 0.5) {
+    return `${arr2[idx1]} ${arr1[idx2]}`;
+  } else {
+    return `${arr1[idx2]} ${arr2[idx1]}`;
+  }
+}
