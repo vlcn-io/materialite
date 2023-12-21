72db789c934a9db7294604d8c04e3484fc44e53f
Matt <1009003+tantaman@users.noreply.github.com>
2023-12-21 11:12:01 -0500

    deploy demos

--- demos/linearite/vite.config.ts
+++ demos/linearite/vite.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   optimizeDeps: {
-    // TODO remove once fixed https://github.com/vitejs/vite/issues/8427
-    exclude: ["@vlcn.io/crsqlite-wasm"],
-    include: ['react', 'react-dom'],
+    include: ["react", "react-dom"],
   },
-  server: {
-    fs: {
-      strict: false,
-    }
-  },
+  base: "/materialite/linearite",
   plugins: [
     react(),
     svgr({
       svgrOptions: {
         svgo: true,
-        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+        plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
         svgoConfig: {
-          plugins: ['preset-default', 'removeTitle', 'removeDesc', 'removeDoctype', 'cleanupIds'],
+          plugins: [
+            "preset-default",
+            "removeTitle",
+            "removeDesc",
+            "removeDoctype",
+            "cleanupIds",
+          ],
         },
       },
     }),
   ],
-})
+});
